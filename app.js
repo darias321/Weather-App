30412209824499fea761f13b86fffb3816624868
Danielle Arias <daniellearias321@gmail.com>
2020-05-13 17:53:32 -0700

    fixed bugs and added styling

--- app.js
+++ app.js
@@ -9,14 +9,17 @@ $(document).ready(function () {
     userInput = $("#userInput").val();
 
     var cities = JSON.parse(localStorage.getItem("cities")) || [];
-    cities.push(userInput);
+
+    if (cities.indexOf(userInput) === -1) {
+      cities.push(userInput);
+    }
 
     localStorage.setItem("cities", JSON.stringify(cities));
 
     $("#cities").html("");
     for (var i = 0; i < cities.length; i++) {
       $("#cities").append(`
-      <li>${cities[i]}</li>  
+      <li class="list-group-item">${cities[i]}</li>  
       `);
       $("li").click(function (event) {
         $("input").val($(this).text());
@@ -61,14 +64,17 @@ $(document).ready(function () {
       method: "GET",
       url: `http://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=c19859ebb61ccce6f0cbf52c315b53a3&units=imperial`,
     }).then(function (response) {
-      for (var i = 0; i < response.list.length; i += 5) {
+      $("#forecast").html("<h3>Forecast</h3>");
+      for (var i = 0; i < response.list.length; i += 8) {
         $("#forecast").append(`
-          <div class="card">
-            <div class="card-body"></div>
-            <p>Temperature: ${response.list[i].main.temp} F</p>
-            <p>Pressure: ${response.list[i].main.pressure}</p>
-            <p>Humidity: ${response.list[i].main.humidity}</p>
-
+          <div class="col-xs-12 col-md-4 col-lg-3">
+            <div class="card">
+              <div class="card-body"></div>
+              <p>Temperature: ${response.list[i].main.temp} F</p>
+              <p>Pressure: ${response.list[i].main.pressure}</p>
+              <p>Humidity: ${response.list[i].main.humidity}</p>
+              <img src="http://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png" />
+            </div>
           </div>
         `);
       }
